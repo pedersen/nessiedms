e3ed1a8c9452af4a7f6e925bb4fb4f6e7cbc2fe2
Michael J. Pedersen <m.pedersen@icelus.org>
2014-02-05 22:25:28 -0500

    Basic TG Quickstart app. Includes mongo, authentication

---
+++ nessiewiki/websetup/schema.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Setup the nessiewiki application"""
+from __future__ import print_function
+
+import logging
+from tg import config
+
+def setup_schema(command, conf, vars):
+    """Place any commands to setup nessiewiki here"""
